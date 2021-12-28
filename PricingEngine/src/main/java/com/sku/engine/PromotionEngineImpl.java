e29c8c0febb78925296adf73a6be2d1f154d2374
rama <jbr120720@gmail.com>
2021-12-28 22:53:19 +0000

    Started the implemention of scenario 1 with test case

---
+++ PricingEngine/src/main/java/com/sku/engine/PromotionEngineImpl.java
@@ -0,0 +1,12 @@
+package com.sku.engine;
+
+import java.util.List;
+
+public class PromotionEngineImpl {
+
+	public double calculatePriceWithApplyPromotion(List<String> asList) {
+		double totalPrice = 100;
+		return totalPrice;
+	}
+
+}
---
+++ PricingEngine/src/test/java/com/sku/engine/PromotionEngineImplTest.java
@@ -0,0 +1,20 @@
+package com.sku.engine;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class PromotionEngineImplTest {
+	
+	@Test
+	public void testScenario1()
+	{
+		double getTotalprice=new PromotionEngineImpl().calculatePriceWithApplyPromotion(
+				Arrays.asList("A", "B", "C"));
+		assertEquals(100.0, getTotalprice,0.0);
+		
+	}
+
+}
